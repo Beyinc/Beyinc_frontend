12ba936fd74ddde9097077dfa1259d04a86f27d9
Rishabh Jambhulkar <88210544+rishabhjambhulkar@users.noreply.github.com>
2024-10-09 21:57:31 +0530

    merged from mentorSearch

--- src/Components/BeyincProfessional/BeyincProfessional.js
+++ src/Components/BeyincProfessional/BeyincProfessional.js
@@ -112,40 +112,7 @@ const BeyincProfessional = () => {
   };
 
   return (
-    // <Box
-    //   display="flex"
-    //   justifyContent="center"
-    //   alignItems="center"
-    //   height="100vh"
-    // >
-    //   <Box
-    //     component="form"
-    //     onSubmit={handleSubmit}
-    //     p={3}
-    //     border={1}
-    //     borderRadius={4}
-    //     borderColor="grey.300"
-    //   >
-    //     <Typography variant="h5" mb={2} align="center">
-    //       Select your role
-    //     </Typography>
 
-    //     <FormControl component="fieldset">
-    //       <RadioGroup value={selectedRole} onChange={handleRoleChange}>
-    //         <FormControlLabel value="Mentor" control={<Radio />} label="Mentor" />
-    //         <FormControlLabel value="Investor" control={<Radio />} label="Investor" />
-    //         <FormControlLabel value="Cofounder" control={<Radio />} label="Cofounder" />
-    //         <FormControlLabel value="InstitutionalInvestor" control={<Radio />} label="Institutional Investor" />
-    //       </RadioGroup>
-    //     </FormControl>
-
-    //     <Box mt={2} textAlign="center">
-    //       <Button type="submit" variant="contained" color="primary">
-    //         Submit
-    //       </Button>
-    //     </Box>
-    //   </Box>
-    // </Box>
 
     <form
       onSubmit={handleSubmit}
