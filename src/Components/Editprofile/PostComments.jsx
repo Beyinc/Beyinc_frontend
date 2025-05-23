047204cc2995f6bd402088bde9a32cb42f10acb4
charishma-reddy-ui <charishmavadla05@gmail.com>
2025-05-23 15:54:23 +0530

    added the functionality of user doc attach

--- src/Components/Editprofile/PostComments.jsx
+++ src/Components/Editprofile/PostComments.jsx
@@ -20,44 +20,31 @@ const PostComments = ({ fetchComments, postId }) => {
   const navigate = useNavigate();
 
   const [allComments, setAllComments] = useState([]);
+  const [attachments, setAttachments] = useState([]);
 
-  const socket = useRef();
-  useEffect(() => {
-    socket.current = io(socket_io);
-  }, []);
-
-  useEffect(() => {
-    if (postId && fetchComments) {
-      ApiServices.getPostComments({ postId: postId })
-        .then((res) => {
-          setAllComments(
-            res.data.sort(
-              (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
-            )
-          );
-        })
-        .catch((err) => {
-          dispatch(
-            setToast({
-              message: "Error Occured",
-              bgColor: ToastColors.failure,
-              visible: "yes",
-            })
-          );
-        });
-    }
-  }, [postId, postTrigger, fetchComments]);
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setAttachments(files);
+  };
 
   const sendText = async () => {
     dispatch(setLoading({ visible: "yes" }));
 
+    const formData = new FormData();
+    formData.append("postId", postId);
+    formData.append("commentBy", user_id);
+    formData.append("comment", comment);
+    formData.append("parentCommentId", undefined);
+
+    // Append each attachment
+    attachments.forEach(file => {
+      formData.append("files", file);
+    });
+
     setComment("");
-    await ApiServices.addPostComment({
-      postId: postId,
-      commentBy: user_id,
-      comment: comment,
-      parentCommentId: undefined,
-    })
+    setAttachments([]);
+
+    await ApiServices.addPostComment(formData)
       .then((res) => {
         setpostTrigger(!postTrigger);
       })
@@ -161,7 +148,8 @@ const PostComments = ({ fetchComments, postId }) => {
                 resize: "none",
               }}
             />
-            <svg
+            {/* Remove this non-working pin SVG */}
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               width="30"
               height="30"
@@ -180,7 +168,7 @@ const PostComments = ({ fetchComments, postId }) => {
                 fill="gray"
                 d="M7.5 18A5.5 5.5 0 0 1 2 12.5A5.5 5.5 0 0 1 7.5 7H18a4 4 0 0 1 4 4a4 4 0 0 1-4 4H9.5A2.5 2.5 0 0 1 7 12.5A2.5 2.5 0 0 1 9.5 10H17v1.5H9.5a1 1 0 0 0-1 1a1 1 0 0 0 1 1H18a2.5 2.5 0 0 0 2.5-2.5A2.5 2.5 0 0 0 18 8.5H7.5a4 4 0 0 0-4 4a4 4 0 0 0 4 4H17V18z"
               />
-            </svg>
+            </svg> */}
             <svg
               onClick={sendText}
               className="send-button-svg"
@@ -205,6 +193,55 @@ const PostComments = ({ fetchComments, postId }) => {
                 stroke-linejoin="round"
               />
             </svg>
+            
+            {/* Add file input */}
+            <label htmlFor="file-input">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                style={{
+                  cursor: "pointer",
+                  padding: "10px",
+                  position: "absolute",
+                  right: "90px",
+                  top: "6px",
+                }}
+              >
+                <path
+                  fill="gray"
+                  d="M16.5 6v11.5c0 2.21-1.79 4-4 4s-4-1.79-4-4V5c0-1.38 1.12-2.5 2.5-2.5s2.5 1.12 2.5 2.5v10.5c0 .55-.45 1-1 1s-1-.45-1-1V6H10v9.5c0 1.38 1.12 2.5 2.5 2.5s2.5-1.12 2.5-2.5V5c0-2.21-1.79-4-4-4S7 2.79 7 5v12.5c0 3.04 2.46 5.5 5.5 5.5s5.5-2.46 5.5-5.5V6h-1.5z"
+                />
+              </svg>
+            </label>
+            <input
+              id="file-input"
+              type="file"
+              multiple
+              accept="image/*,video/*"
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+            />
+            
+            {/* Preview attachments */}
+            {attachments.length > 0 && (
+              <div className="attachments-preview">
+                {attachments.map((file, index) => (
+                  <div key={index} className="attachment-item">
+                    {file.type.startsWith('image/') ? (
+                      <img src={URL.createObjectURL(file)} alt="attachment" />
+                    ) : (
+                      <video src={URL.createObjectURL(file)} controls />
+                    )}
+                    <button onClick={() => setAttachments(prev => prev.filter((_, i) => i !== index))}>
+                      Remove
+                    </button>
+                  </div>
+                ))}
+              </div>
+            )}
+            
           </div>
         </div>
       </div>
